327caf08f59e18039b349e2f6d68f12f3148280d
rhuangalvao <rhuangalvao99@gmail.com>
2019-07-12 17:01:51 -0300

    Update memoria.js

--- js/memoria.js
+++ js/memoria.js
@@ -2,13 +2,18 @@ var config = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
+	backgroundColor: '#7f7196',
 	scene: {
 		preload: preload,
 		create: create,
 		update: update
 	}
 };
+
 var game = new Phaser.Game(config);
+var cor;
+var tamanho;
+var algoritmo;
 
 function preload ()
 {	
@@ -17,13 +22,42 @@ function preload ()
 
 function create ()
 {
+	// qual algoritmo
+	//var infoalgoritmo = this.add.text(660, 180, 'bestfit', { fill: '#000000' });
 	
+	// paredes do jogo
+	var par1 = new Phaser.Geom.Rectangle(0, 0, 20, 600);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par1);
+	var par2 = new Phaser.Geom.Rectangle(0, 0, 800, 20);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par2);
+	var par3 = new Phaser.Geom.Rectangle(780, 0, 20, 600);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par3);
+	var par4 = new Phaser.Geom.Rectangle(0, 580, 800, 20);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par4);
+	var par5 = new Phaser.Geom.Rectangle(600, 0, 20, 600);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par5);
+	var par6 = new Phaser.Geom.Rectangle(600, 140, 200, 20);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par6);
+	var par7 = new Phaser.Geom.Rectangle(600, 220, 200, 20);
+    var graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+    graphics.fillRectShape(par7);
+	
+	
+	var novapeca = new Phaser.Geom.Rectangle(640, 80, 40, 40);
+	var graphics = this.add.graphics({ fillStyle: { color: cor } });
+	graphics.fillRectShape(novapeca);
 }
 
 function update ()
 {
+	
 }
-
 
 
 //Classe memória
@@ -107,3 +141,46 @@ class Memoria{
         //resta implementar a remoção visual da peça;
     }       
 }
+/*
+	criarEExibirPeca(){
+		var novapeca = new Phaser.Geom.Rectangle(640, 80, tamanho, 40);
+		var graphics = this.add.graphics({ fillStyle: { color: cor } });
+		graphics.fillRectShape(novapeca);
+	}
+
+
+	sortearTamanhoCorEAlgoritmo(){
+			int i = Math.floor(Math.random() * 5);
+			//vermelho
+			if(i == 0){
+				cor = 0xff0000;
+			//verde
+			}else if(i == 1){
+				cor = 0x00ff00;
+			//amarelo
+			}else if(i == 2){
+				cor = 0xff0000;
+			//roxo
+			}else if(i == 3){
+				cor = 0x5a005a;
+			//laranja
+			}else if(i == 4){
+				cor = 0xff8c00;
+			}
+			i = Math.floor(Math.random() * 2);;
+			if(i == 0){
+				algoritmo = 'Bestfit';
+			}else if(i == 1){
+				algoritmo = 'Worstfit';
+			}
+			i = Math.floor(Math.random() * 3);
+			if(i == 0){
+				tamanho = 40;
+			}else if(i == 1){
+				tamanho = 80;
+			}else if(i == 2){
+				tamanho = 120;
+			}		
+			var infoalgoritmo = this.add.text(660, 180, algoritmo, { fill: '#000000' });
+	}
+*/	
